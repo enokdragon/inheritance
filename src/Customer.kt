5ba6418690bdc8903cced4110cb8c9753f833ef0
enokdragon <111323615+enokdragon@users.noreply.github.com>
2024-03-05 12:36:42 -0600

    Update Customer.kt
    added comment block

--- src/Customer.kt
+++ src/Customer.kt
@@ -1,3 +1,10 @@
+/**
+ * @author Enok Dragon
+ * Class Customer
+ * @param
+ * customer name, customer address, customer phone number, square footage of yard
+ * 3/5/2024
+ */
 open class Customer(customerName: String,
                     customerAddress: String,
                     customerPhone: String,
